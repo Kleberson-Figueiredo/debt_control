1600fe517a8a859fcfb160d74a3fbfa957beaa58
Kleberson-Figueiredo <kleberson.figueiredo.kf@gmail.com>
2025-09-17 18:49:23 -0400

    update firebase

--- debt_control/utils/firebase.py
+++ debt_control/utils/firebase.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import firebase_admin
@@ -10,7 +9,7 @@ load_dotenv()
 firebase_key_path = os.getenv('FIREBASE_CREDENTIALS')
 
 if firebase_key_path:  # só inicializa se a variável estiver definida
-    cred = credentials.Certificate(json.loads(firebase_key_path))
+    cred = credentials.Certificate(firebase_key_path)
     firebase_admin.initialize_app(cred)
 else:
     # Modo teste/CI: não inicializa Firebase
